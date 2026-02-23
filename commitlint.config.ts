a4b0b232e23a637b72321d6280e853d34a992640
decante <gustavocamposantunes@gmail.com>
2026-02-23 19:46:17 -0300

    feat: add header-max-length rule to commitlint configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -3,7 +3,9 @@ import type { UserConfig } from '@commitlint/types'
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   formatter: '@commitlint/format',
-  rules: {},
+  rules: {
+    'header-max-length': [2, 'always', 120],
+  },
 }
 
 export default Configuration
