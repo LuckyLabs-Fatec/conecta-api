a6a65ddf1bbeb37d8fe2fffcbd118ebf66aee5b3
decante <gustavocamposantunes@gmail.com>
2026-02-26 19:20:23 -0300

    test: ensure user is returned if email exists

--- src/infra/repositories/TypeOrmUserRepository.spec.ts
+++ src/infra/repositories/TypeOrmUserRepository.spec.ts
@@ -1,16 +1,33 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
+
+import { UserEntity } from "../database/typeorm/entities/UserEntity";
+import { TestDataSource } from "../database/typeorm/test-data-source";
 
 import { TypeOrmUserRepository } from "./TypeOrmUserRepository";
 
 describe("TypeOrmUserRepository", () => {
     let sut: TypeOrmUserRepository;
 
-    beforeEach(() => {
-        sut = new TypeOrmUserRepository();
+    beforeAll(async () => {
+        await TestDataSource.initialize();
+
+        sut = new TypeOrmUserRepository(TestDataSource);
     });
 
     it("should return null if email does not exist", async () => {
         const user = await sut.findByEmail("nonexistent@example.com");
         expect(user).toBeNull();
+    });
+
+    it("should return user if email exists", async () => {
+        const repo = TestDataSource.getRepository(UserEntity);
+        await repo.save({
+            email: "existent@example.com",
+            passwordHash: "hashedpassword",
+            name: "Test User",
+        });
+
+        const user = await sut.findByEmail("existent@example.com");
+        expect(user?.email).toBe("existent@example.com");
     });
 });
