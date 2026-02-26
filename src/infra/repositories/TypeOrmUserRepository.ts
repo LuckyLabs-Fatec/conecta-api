a751a670f4c6673c6f877316ac394a7cc5333fc5
decante <gustavocamposantunes@gmail.com>
2026-02-26 19:23:29 -0300

    feat: implement TypeOrmUserRepository with findByEmail method

--- src/infra/repositories/TypeOrmUserRepository.ts
+++ src/infra/repositories/TypeOrmUserRepository.ts
@@ -1,6 +1,23 @@
-export class TypeOrmUserRepository {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    async findByEmail(email: string) {
-        return null;
+import { DataSource, Repository } from "typeorm";
+
+import { UserEntity } from "../database/typeorm/entities/UserEntity";
+
+import { UserRepository } from "@/domain/repositories/UserRepository";
+
+export class TypeOrmUserRepository implements UserRepository{
+    private respository: Repository<UserEntity>;
+
+    constructor(dataSource: DataSource) {
+        this.respository = dataSource.getRepository(UserEntity);
+    }
+    
+    async findByEmail(email: string): Promise<UserEntity | null> {
+        const userEntity = await this.respository.findOne({ where: { email } });
+
+        if(!userEntity) {
+            return null;
+        }
+
+        return userEntity;
     }
 }
