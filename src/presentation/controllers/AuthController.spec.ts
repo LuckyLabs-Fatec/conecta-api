7bd4815252b05b26818e95d0b95a966aeffdc0ef
decante <gustavocamposantunes@gmail.com>
2026-03-02 20:08:08 -0300

    test: ensure AuthController return 401 when user if not found

---
+++ src/presentation/controllers/AuthController.spec.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+import { AuthController } from "./AuthController";
+
+import { AuthenticateUser } from "@/domain/usecases/auth/AuthenticateUser";
+import { InvalidCredentialsError } from "@/domain/errors/InvalidCredentialsError";
+
+
+
+describe("AuthController", () => {
+    let authController: AuthController;
+    let authenticateUserMock: AuthenticateUser;
+
+    beforeEach(() => {
+        authenticateUserMock = {
+            execute: vi.fn(),
+        } as unknown as AuthenticateUser;
+
+        authController = new AuthController(authenticateUserMock);
+    })
+
+    it("should return 401 when user is not found", async () => {
+        vi.mocked(authenticateUserMock.execute).mockRejectedValue(new InvalidCredentialsError());
+
+        const req = {
+            body: {
+                email: "notfound@example.com",
+                password: "any-password",
+            }
+        } as Request;
+
+        const res = {
+            status: vi.fn().mockReturnThis(),
+            json: vi.fn(),
+        } as unknown as Response;
+
+        await authController.login(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.json).toHaveBeenCalledWith({ message: "Invalid credentials" });
+    })
+});
