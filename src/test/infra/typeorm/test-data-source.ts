b01e7f1ec5b5a0411658229cef27ede4a1714d95
decante <gustavocamposantunes@gmail.com>
2026-03-23 19:13:57 -0300

    fix: update import path for UserEntity in test data source

--- src/test/infra/typeorm/test-data-source.ts
+++ src/test/infra/typeorm/test-data-source.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 
-import { UserEntity } from "../../../infra/database/typeorm/entities/UserEntity";
+import { UserEntity } from "@/infra/database/typeorm/entities/UserEntity";
 
 export const TestDataSource = new DataSource({
   type: "sqljs",
